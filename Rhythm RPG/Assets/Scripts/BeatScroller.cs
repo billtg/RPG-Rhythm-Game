6e900798b79cba2e28d542e1433ff77993019514
Graham Tattersall <billtg@users.noreply.github.com>
2019-01-28 15:21:23 -0330

    v0.0.01 - Initial Experiment and beaat fixing
    Initial pass at following a rhythm game tutorial, plus attempted Conductor changes made from Yu Chao's Rhythm game tutorial.
    
    Still need to update main code to fix on dsptime, and use to drive the game and beats.

---
+++ Rhythm RPG/Assets/Scripts/BeatScroller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatScroller : MonoBehaviour {
+
+    public float beatTempo;
+
+    public bool hasStarted;
+
+	// Use this for initialization
+	void Start () {
+        beatTempo = beatTempo / 60f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted)
+        {
+            //Press any button to start
+            //if (Input.anyKeyDown)
+            //{
+            //    Debug.Log("Starting!");
+            //    hasStarted = true;
+            //}
+        } else
+        {
+            MoveNote();
+        }
+	}
+
+    void MoveNote()
+    {
+        transform.position -= new Vector3(0f,beatTempo*Time.deltaTime,0f);
+    }
+}
---
+++ Rhythm RPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public AudioSource theMusic;
+    public bool startPlaying;
+    public BeatScroller bs;
+
+    public int currentScore;
+    public int scorePerNote = 100;
+    public int currentMultiplier;
+    public int multiplierTracker;
+    public int[] multiplierThresholds;
+
+    public Text scoreText;
+    public Text multiText;
+
+    public static GameManager instance;
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+
+        currentMultiplier = 1;
+        scoreText.text = "Score: 0";
+        multiText.text = "Multipler: x1";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (!startPlaying)
+        {
+            if (Input.anyKeyDown)
+            {
+                startPlaying = true;
+                bs.hasStarted = true;
+
+                theMusic.Play();
+            }
+        }
+	}
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit on time");
+
+        currentScore += scorePerNote * currentMultiplier;
+
+        if (currentMultiplier - 1 < multiplierThresholds.Length)
+        {
+            multiplierTracker++;
+
+            if (multiplierThresholds[currentMultiplier - 1] < multiplierTracker)
+            {
+                multiplierTracker = 0;
+                currentMultiplier++;
+            }
+        }
+        
+
+
+        scoreText.text = "Score: " + currentScore.ToString();
+        multiText.text = "Multipler: x" + currentMultiplier.ToString();
+    }
+
+    public void NoteMissed()
+    {
+        Debug.Log("Missed Note");
+        currentMultiplier = 1;
+        multiplierTracker = 0;
+        multiText.text = "Multipler: x" + currentMultiplier.ToString();
+    }
+}
